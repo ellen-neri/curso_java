4b5ad5249573f17aa9989ceb79e37fd2a93f3a97
ellen-neri <ellenandrade.n@gmail.com>
2024-02-23 10:53:29 +0000

    debug

--- estruturas_repetitivas/src/Main.java
+++ estruturas_repetitivas/src/Main.java
@@ -1,24 +1,49 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
 	
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+//		Locale.setDefault(Locale.US);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		double largura = sc.nextDouble();
+//		double comprimento = sc.nextDouble();
+//		double metroQuadrado = sc.nextDouble();
+//	
+//		double area = largura * comprimento;
+//		double preco = area * metroQuadrado;
+//	
+//		System.out  .printf("ÁREA = %.2f%n", area);
+//		System.out.printf("PREÇO = %.2f%n", preco);
+//	
+//		sc.close();
 		
-		double largura = sc.nextDouble();
-		double comprimento = sc.nextDouble();
-		double metroQuadrado = sc.nextDouble();
-	
-		double area = largura * comprimento;
-		double preco = area * metroQuadrado;
-	
-		System.out  .printf("ÁREA = %.2f%n", area);
-		System.out.printf("PREÇO = %.2f%n", preco);
-	
-		sc.close();
+		
+//        Scanner sc1 = new Scanner(System.in);
+//
+//        int x = sc1.nextInt();
+//
+//        while (x != 0) {
+//             x = sc1.nextInt();
+//        }
+//
+//        sc1.close();
+
+
+        Scanner sc2 = new Scanner(System.in);
+
+        int x1 = sc2.nextInt();
+        int soma = 0;
+
+        while (x1 != 0) {
+            soma += x1;
+            x1 = sc2.nextInt();
+        }
+
+        System.out.println(soma);
+
+        sc2.close();
 		
 	}
 
